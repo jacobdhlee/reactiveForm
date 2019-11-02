49a91369801e1a28bd7a0368bc5f8ac19f9e1152
Donghee Lee <jacobdhlee@gmail.com>
2019-11-02 13:36:12 -0400

    add checkbox

--- src/component/SelectField.js
+++ src/component/SelectField.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Select from 'react-select';
 
 const SelectField = (props) => {
-  console.log('props : ', props)
+  const option = props.subInput.map(item => {
+    return {label: item.name, value: item.value}
+  })
   return (
     <div>
       Select Field
+      <div>{props.label}</div>
+      <Select options={option} placeholder={'please select'}/>
     </div>
   )
 }
--- src/component/input/Checkbox.js
+++ src/component/input/Checkbox.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const Checkbox = (props) => {
-  console.log('props in checkbox ', props)
   return(
     <div>
-      checkbox
+      <div>{props.label}</div>
+      {props.subInput.map((item, i) => {
+        let value = item.value === 'true' ? true : false
+        return (
+          <div key={i}>
+            <div>{item.name}</div>
+            <input type={'checkbox'} name={item.name} checked={value}/>
+          </div>
+        )
+      })}
     </div>
   )
 };
